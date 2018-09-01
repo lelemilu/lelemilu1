989b8643252c0b0a60703836ceb3f2e6e6666e91
lelemilu <42897037+lelemilu@users.noreply.github.com>
2018-09-02 02:29:11 +0800

    Add files via upload

---
+++ Helloworld1.java
@@ -0,0 +1,7 @@
+public class Helloworld1 {
+            public static void main (String[]args){
+                        System.out.println("Helloworld1");
+		int $123=3;
+            }
+                
+}
